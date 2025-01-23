080b7c78f9f519617a2b6a4887fdad49f36c8fa4
mohammed zato <zatomohammed231@gmail.com>
2025-01-23 12:48:26 +0000

    changed styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Die from './component/Die'
 function App() {
 
   return (
-    <div class="m-8 px-4 py-4 border border-indigo-400 rounded lg:max-w-xl lg:mx-auto shadow-xl">
+    <div class="pt-16 px-4 border bg-gray-50 h-screen rounded lg:max-w-xl lg:mx-auto shadow-xl">
       <Die />
     </div>
   )
