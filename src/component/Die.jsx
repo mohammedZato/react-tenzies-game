5774f6b30f1480867186a37dc39144cd519c5c53
mohammed zato <zatomohammed231@gmail.com>
2025-01-23 03:19:08 +0000

    updated styling

--- src/component/Die.jsx
+++ src/component/Die.jsx
@@ -73,10 +73,10 @@ export default function Die() {
                     Click each die to freeze it at its current value between rolls.</p>
             }
             {tenzies && <Confetti />}
-            <div class="grid grid-cols-5 gap-4 p-5">
+            <div class="grid grid-cols-5 gap-4 mx-4 py-6">
                 {diceElements}
             </div>
-            <button onClick={rollDice} class="mt-10 rounded-lg focus:outline-none cursor-pointer bg-sky-800 px-10 py-2 hover:bg-sky-700 text-white text-lg font-medium">
+            <button onClick={rollDice} class="mt-8 mb-2 rounded-lg focus:outline-none cursor-pointer bg-sky-800 px-10 py-3 hover:bg-sky-700 hover:scale-105 transition-transform duration-700 text-white text-lg font-medium">
             {tenzies ? "New Game" : "Roll"}
             </button>
         </main>
--- src/component/index.jsx
+++ src/component/index.jsx
@@ -5,7 +5,7 @@ export default function DieElt(props) {
         backgroundColor: props.isHeld ? "#a3e635" : "#6b7280"
     }
     return (
-        <div onClick={() => props.holdDice(props.id)} style={styles} class=" flex justify-center items-center font-bold text-3xl border bg-gray-300 w-16 h-16 rounded-xl cursor-pointer">
+        <div onClick={() => props.holdDice(props.id)} style={styles} class=" flex justify-center items-center font-bold text-3xl text-gray-black border bg-gray-300 size-[52px] rounded-xl cursor-pointer sm:size-16">
             <h2>{props.value}</h2>
         </div>
     )
